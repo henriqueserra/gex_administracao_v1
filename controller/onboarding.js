719b8a8a0a25c4cdd489e1c8705d6d4d8ffff1cd
Henrique Ribeiro Serra <gex.gerenteexpresso@gmail.com>
2020-12-28 17:54:17 -0300

    20201228-003

--- controller/onboarding.js
+++ controller/onboarding.js
@@ -2,7 +2,13 @@ const mongo = require("../library/mongo.js");
 
 module.exports = (app) => {
   app.get("/onboarding/criadb", async (requisicao, resposta) => {
-    const resultado = await mongo.criaBancoECollections(requisicao.query.id);
-    resposta.status(200).send(resultado);
+    if (requisicao.query.nomedb) {
+      const resultado = await mongo.criaBancoECollections(
+        requisicao.query.nomedb
+      );
+      resposta.status(200).send(resultado);
+    } else {
+      resposta.status(401).json({ erro: "Requisição mal formada" });
+    }
   });
 };
